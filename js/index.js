68087d245ceeadeee4c00373732582e4ff03d73a
godkind73-Arpa <godkind73@gmail.com>
2025-10-23 10:21:08 +0600

    Created  two index.html for different folders and one js file.

---
+++ js/index.js
@@ -0,0 +1,59 @@
+// window.alert('Hello from external JS');
+// alert('Hello from external JS without writing window object');
+console.log('HEllo from console JS');
+document.writeln("Hello World");
+
+let a; //declaration
+//data type initialization
+//data type
+let studentId = 123;
+let studentcgpa = 3.75;
+let studentName = 'AAsdds';
+let eS =""; //empty string
+
+
+//print
+document.writeln('student ID :' + studentId + '<br>');
+document.writeln('student CGPA :' + studentcgpa + '<br>');
+document.writeln('student Name :' + studentName +'<br>');
+
+
+//console print
+console.log('student ID:'+ studentId);
+console.log('student CGPA:'+ studentcgpa);
+console.log('student Name:'+ studentName);
+
+document.writeln('<p style = "color:red"> Arpa Sen </p>');
+document.writeln('<p style = "color:blue"> '+ studentName +'</p>');
+
+
+//type casting
+//Number
+let cgpa = parseInt(studentcgpa);
+document.writeln(cgpa);
+
+//Integer to Float
+let Id = parseFloat(studentId)/12;
+document.writeln('Integer to Float:' + typeof(id));
+document.writeln(cgpa.toFixed(3));
+
+//string to Number
+let bS = parseInt(b);
+document.writeln('String to Number:')
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
